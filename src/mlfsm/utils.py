2dd42d7789997866af05ecf4953fdda916520303
joegomes <joegomes@stanford.edu>
2025-12-18 15:26:50 -0600

    Add load_xyz_fixed function

--- src/mlfsm/utils.py
+++ src/mlfsm/utils.py
@@ -5,8 +5,10 @@ from pathlib import Path
 import numpy as np
 from ase import Atoms
 from ase.io import read
+from ase.constraints import FixAtoms
+from numpy.typing import NDArray
 
-from mlfsm.geom import project_trans_rot
+from mlfsm.geom import project_trans_rot, project_trans_rot_fixed
 
 
 def load_xyz(reaction_dir: Path | str) -> tuple[Atoms, Atoms]:
@@ -27,6 +29,31 @@ def load_xyz(reaction_dir: Path | str) -> tuple[Atoms, Atoms]:
 
     return reactant, product
 
+def load_xyz_fixed(reaction_dir: Path | str, fixed: NDArray[int]) -> tuple[Atoms, Atoms]:
+    """
+    Load reactant and product geometries from a directory.
+
+    Assumes a file named initial.xyz containing the reactant and product geometries.
+    """
+    xyz = Path(reaction_dir) / "initial.xyz"
+    if not xyz.is_file():
+        raise Exception(f"Input file {xyz} not found.")
+    atoms = read(xyz, format="xyz", index=":")
+    reactant = atoms[0]
+    product = atoms[-1]
+    if len(fixed) == 0:
+        r_xyz, p_xyz = project_trans_rot(reactant.get_positions(), product.get_positions())
+        reactant.set_positions(r_xyz.reshape(-1, 3))
+        product.set_positions(p_xyz.reshape(-1, 3))
+    else:
+        c = FixAtoms(indices=fixed)
+        r_xyz, p_xyz = project_trans_rot_fixed(reactant.get_positions(), product.get_positions(), fixed)
+        reactant.set_positions(r_xyz.reshape(-1, 3))
+        reactant.set_constraint(c)
+        product.set_positions(p_xyz.reshape(-1, 3)) 
+        product.set_constraint(c)
+
+    return reactant, product
 
 def float_check(x: float) -> float:
     """
